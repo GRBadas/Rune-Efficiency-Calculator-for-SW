18365a09f90531c488aadeead96ed49ab521b843
Gabriel Badaró <grbadas@gmail.com>
2023-04-09 14:53:19 -0300

    Rune management project

--- src/main/java/com/badas/springboot/repository/TasksRepository.java
+++
@@ -1,8 +0,0 @@
-package com.badas.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.badas.springboot.model.Tasks;
-
-public interface TasksRepository extends JpaRepository<Tasks, Long>{
-
-}
